3e96bb19355f1d47a0de3cf5e464f86daeaf84e3
Matiboy <mathieu.chauvinc@gmail.com>
2013-07-29 06:45:50 +0800

    Chaining .then and rejection

--- app/scripts/controllers/qThen.js
+++ app/scripts/controllers/qThen.js
@@ -3,23 +3,44 @@
 angular.module('deferredApp')
 	.controller('QThenCtrl', function ($scope, $q, $http) {
 		// $http returns a promise
-		var httpPromise = $http.get('http://maps.googleapis.com/maps/api/geocode/json?address=Somerset,%20Kuala%20Lumpur,%20Malaysia&sensor=false');
+		var httpPromise = $http.get('http://randomthings.blob');
+		// var httpPromise = $http.get('http://maps.googleapis.com/maps/api/geocode/json?address=Somerset,%20Kuala%20Lumpur,%20Malaysia&sensor=false');
 		// The promise gets resolved with the full http response, including headers, etc.
 		httpPromise.then(function(fullResponse){
 			console.debug(fullResponse);
-		});
-
-		$scope.address = httpPromise;
-
-		// Using .then, we can arrange the data to our preference
-		var modifiedDataPromise = httpPromise.then(function(fullResponse){
-			// The new promise created by .then is resolved with the return value of this function
-			return fullResponse.data.results[0].formatted_address;
-		});
-		modifiedDataPromise.then(function(data){
-			console.debug(data);
 		}, function(err){
-
+			console.error('In first then', err);
 		});
-		$scope.betterAddress = modifiedDataPromise;
+
+		// Using .then to propagate rejection
+		var modifiedDataPromise = httpPromise.then(function(fullResponse){
+		}, function(err){
+			console.error('In second then', err); 
+			return err;
+		});
+		// Maybe surprisingly, if we return something in the previous error callback, 
+		// the new promise is _resolved_ with that return value
+		modifiedDataPromise.then(function(data){
+			console.debug('In success callback of new promise', data);
+		}, function(err){
+			console.error('In error callback of new promise', err);
+		});
+
+		// // Rejecting new promise
+		// var rejectingPromise = httpPromise.then(function(fullResponse){
+		// }, function(err){
+		// 	console.error('In second then', err); 
+		// 	// This is how to reject new promise
+		// 	throw {
+		// 		success: false,
+		// 		reason: err
+		// 	}
+		// });
+		// // Maybe surprisingly, if we return something in the previous error callback, 
+		// // the new promise is _resolved_ with that return value
+		// modifiedDataPromise.then(function(data){
+		// 	console.debug('In success callback of new promise', data);
+		// }, function(err){
+		// 	console.error('In error callback of new promise', err);
+		// });
 	});
