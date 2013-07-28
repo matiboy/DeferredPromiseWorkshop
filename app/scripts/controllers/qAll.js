d75f4793820a20ddea909de167a7b66a820ff1b5
Matiboy <mathieu.chauvinc@gmail.com>
2013-07-29 06:10:27 +0800

    Example of $q.all with rejected deferred

--- app/scripts/controllers/qAll.js
+++ app/scripts/controllers/qAll.js
@@ -8,23 +8,29 @@ angular.module('deferredApp')
 		var loadingGPlusFeed = $q.defer();
 		// Fake an Ajax call with random duration
 		$timeout(function() {
-			loadingTwitterFeed.resolve('Twitter loaded');
+			loadingTwitterFeed.reject('Twitter failed to load');
 		}, Math.random() * 5000 );
 		$timeout(function() {
 			loadingGPlusFeed.resolve('Google+ loaded');
 		}, Math.random() * 5000 );
 		loadingTwitterFeed.promise.then(function(res){
 			$scope.twitter = res;
+		}, function(err){
+			console.error(err);
 		});
 		loadingGPlusFeed.promise.then(function(res){
 			$scope.gplus = res;
+			console.debug(res);
 		});
 
 		// Guaranteed to only display after both have been loaded
 		var bothPromise = $q.all([loadingTwitterFeed.promise, loadingGPlusFeed.promise]);
 		// $q.all returns a promise object!
 		bothPromise.then(function(results) {
-			console.debug(results);
 			$scope.both = 'Both have been loaded';
+		}, function(res){
+			console.debug(res);
+			$scope.both = 'This is the message from rejected promise: ' + res;
 		});
+		// Note that this works like a && and gets rejected at the first sign of trouble
 	});
