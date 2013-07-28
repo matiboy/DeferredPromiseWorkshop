f5bbf72392d8d391c32fa022d729a3939cdb6bc9
Matiboy <mathieu.chauvinc@gmail.com>
2013-07-29 04:48:23 +0800

    Basic example of deferred objects, created manually and with $http

--- app/scripts/controllers/singleDeferred.js
+++ app/scripts/controllers/singleDeferred.js
@@ -1,10 +1,37 @@
 'use strict';
 
 angular.module('deferredApp')
-  .controller('SingleDeferredCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+	.controller('SingleDeferredCtrl', function ($scope, $q, $http) {
+		// $http uses the promise API
+		var httpPromise = $http.get('http://angularjs.com');
+		httpPromise.then(function(fullResponse){
+			console.debug(fullResponse);
+		}, function(error){
+			console.error(error);
+		});
+		var wrongUrlPromise = $http.get('http://angularjs.pt');
+		wrongUrlPromise.then(function(fullResponse){
+			console.debug(fullResponse);
+		}, function(error){
+			console.error(error);
+		});
+
+		// Manually creating a deferred object
+		var manualDefer = $q.defer();
+		manualDefer.promise.then(function(resolvedWith){
+			console.debug(resolvedWith);
+		}, function(rejectedWith) {
+			console.error(rejectedWith);
+		});
+		// Resolve the object
+		manualDefer.resolve('Everything s fine');
+
+		// Manually creating a deferred object
+		var soonToBeRejectedDefer = $q.defer();
+		soonToBeRejectedDefer.promise.then(function(resolvedWith){
+			console.debug(resolvedWith);
+		}, function(rejectedWith) {
+			console.error(rejectedWith);
+		});
+		soonToBeRejectedDefer.reject('Oops something went wrong');
+	});
