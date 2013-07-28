1997101f34b9a0d4b845c024b65c4f0e664a427b
Matiboy <mathieu.chauvinc@gmail.com>
2013-07-28 19:40:43 +0800

    Create singleDeferred route

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,6 +7,10 @@ angular.module('deferredApp', [])
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/singleDeferred', {
+        templateUrl: 'views/singleDeferred.html',
+        controller: 'SingleDeferredCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
