67f63ca75db2959f29b43d03fe168137be01586b
Matiboy <mathieu.chauvinc@gmail.com>
2013-08-02 09:44:04 +0800

    Put everything into main

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,33 @@
 'use strict';
 
 angular.module('deferredApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function ($scope, $q, $http) {
+		// Resolving only accepts a single value
+		var manualDefer = $q.defer();
+		manualDefer.promise.then(function(resolvedWith, moreInfo){
+			console.debug(resolvedWith, moreInfo);
+		}, function() {
+		});
+		// Resolving only accepts a single value
+		manualDefer.resolve('I can only resolve with one value', 'Not 2');
+
+		// Of course, that value could be an array
+		// var passingMoreInfo = $q.defer();
+		// passingMoreInfo.promise.then(function(resolvedWith){
+		// 	console.debug(resolvedWith);
+		// }, function() {
+		// });
+		// passingMoreInfo.resolve(['There', 'you', 'go']);
+
+		// Or anything else
+		// var resolvingWithACallback = $q.defer();
+		// resolvingWithACallback.promise.then(function(cb){
+		// 	cb();
+		// }, function() {
+		// });
+		// resolvingWithACallback.resolve(angular.bind(console, console.log, 'Hi!'));
+		// Same effect as 
+		// resolvingWithACallback.resolve(function() {
+		//	console.log('Hi!');
+		// })
+	});
