0b5e6cb67c03e39aac899ee79020b32e3e1fa4c7
Matiboy <mathieu.chauvinc@gmail.com>
2013-08-02 10:25:42 +0800

    Move everything into main

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,31 @@
 'use strict';
 
 angular.module('deferredApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl', function ($scope, $q, $http) {
+  		// $http returns a promise
+		var httpPromise = $http.get('http://maps.googleapis.com/maps/api/geocode/json?address=Somerset,%20Kuala%20Lumpur,%20Malaysia&sensor=false');
+		// The promise gets resolved with the full http response, including headers, etc.
+		// httpPromise.then(function(fullResponse){
+		// 	console.debug(fullResponse);
+		// });
+
+		// httpPromise.success(function(data) {
+		// 	console.log(data);
+		// 	$scope.dataOnly = data;
+		// })
+
+		// $scope.response = httpPromise;
+
+		// Using .then, we can arrange the data to our preference
+		var modifiedDataPromise = httpPromise.then(function(fullResponse){
+			console.debug(fullResponse);
+			// The new promise created by .then is resolved with the return value of this function
+			return fullResponse.data.results[0].formatted_address;
+		});
+		modifiedDataPromise.then(function(data){
+			console.debug(data);
+		}, function(err){
+
+		});
+		$scope.addressOnly = modifiedDataPromise;
+	});
